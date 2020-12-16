939f594c0bad6cca52469c75b142410510664380
ShokiHiraga <chaser.cb750@gmail.com>
2020-12-16 07:26:20 +0000

    first commit

---
+++ TEST.java
@@ -0,0 +1,13 @@
+public class TEST {
+	public static void main(String[] args) {
+//	int a = new java.util.Scanner(System.in) .nextInt();
+	int a = 1;
+	    while (a == 8) {
+	    	a++;
+		System.out.println("hello, world!");
+		System.out.println(a);	        
+	    }
+	    
+    System.out.println("ループから抜け出した");
+	}
+}
